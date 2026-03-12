cc5213264e3816193d4b6aa7edcc2543a1a96d18
Hunter Scholz <scholzh@oregonstate.edu>
2026-03-11 19:08:12 -0700

    vessel spec & implementation

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,18 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :volume, :name
 
   def initialize(name, volume)
     @name = name
     @volume = volume
+    @full = false
+  end
+
+  def fill()
+    @full = true
+  end
+
+  def empty?()
+    return !@full
   end
 
 end
