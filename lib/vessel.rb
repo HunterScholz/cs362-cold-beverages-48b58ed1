31093100cc175542df56ca6858c10a5a893d5386
Hunter Scholz <scholzh@oregonstate.edu>
2026-03-11 19:23:40 -0700

    more vessel imp & spec, plus item spec

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,18 +1,25 @@
 class Vessel
-  attr_reader :volume, :name
+  attr_reader :volume, :name, :liquid
 
   def initialize(name, volume)
     @name = name
     @volume = volume
     @full = false
+    @liquid = nil
   end
 
-  def fill()
+  def fill(liquid)
     @full = true
+    @liquid = liquid
   end
 
   def empty?()
     return !@full
+  end
+
+  def clear()
+    @full = false
+    @liquid = nil
   end
 
 end
--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -8,14 +8,30 @@ describe 'A vessel for holding liquid' do
     expect(vessel.volume).to eq(100)
   end
 
-  it 'is initially empty' do
+  it 'is initially empty with no liquid' do
     vessel = Vessel.new('FAKE', 100)
     expect(vessel).to be_empty
+    expect(vessel.liquid).to eq(nil)
   end
 
   it 'is no longer empty when we fill it' do
     vessel = Vessel.new('FAKE', 100)
-    vessel.fill
+    vessel.fill('FAKE LIQUID')
     expect(vessel).to_not be_empty
   end
+
+  it 'has a liquid when filled' do
+    vessel = Vessel.new('FAKE', 100)
+    vessel.fill('FAKE LIQUID')
+    expect(vessel.liquid).to eq('FAKE LIQUID')
+  end
+
+  it 'becomes empty and has no liquid when cleared' do
+    vessel = Vessel.new('FAKE', 100)
+    vessel.fill('FAKE LIQUID')
+    vessel.clear()
+    expect(vessel).to be_empty
+    expect(vessel.liquid).to eq(nil)
+  end
+
 end
